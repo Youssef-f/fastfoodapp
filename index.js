dabf6993e25711d9856ba5317692ea38726d57fb
YFAKIR <youssef.fakir@ynov.com>
2024-03-07 20:48:57 +0100

    add swagger

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+
+const swaggerDocument = YAML.load("./swagger.yaml");
 dotenv.config();
 const URL = process.env.MONGO_URL;
 mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -13,7 +17,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const port = process.env.PORT || 5000;
 const app = express();
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.static(path.join(__dirname, "front-end")));
 app.use(cors());
 app.use(express.json());
